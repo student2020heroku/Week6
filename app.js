484c797678ff321f3e9788166168f9b6f9a1cf13
moskalev27 <moskalev27@mail.ru>
2020-11-23 20:20:51 +0000

    Added OPTIONS in HEADERS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ export default function(express, bodyParser, fs, crypto, http) {
     const app = express();
     const CORS = {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE',
         'Access-Control-Allow-Headers': 'Content-Type, Accept, Access-Control-Allow-Headers',
         'Content-Type': 'text/plain; charset=utf-8'
     };
@@ -50,7 +50,7 @@ export default function(express, bodyParser, fs, crypto, http) {
                 res.send(login);
             }
         })
-        .all('/*/', (req, res) => {
+        .all('/*', (req, res) => {
             res.set(CORS);
             res.send(login);
         });

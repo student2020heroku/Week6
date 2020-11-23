d13332250fcbb93aa3e3842ecd81642d2ac2aa7b
moskalev27 <moskalev27@mail.ru>
2020-11-23 21:08:28 +0000

    6

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ export default function(express, bodyParser, fs, crypto, http) {
     const app = express();
     const CORS = {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE,HEAD',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE',
         'Access-Control-Allow-Headers': 'Content-Type, Accept, Access-Control-Allow-Headers',
         'Content-Type': 'text/plain; charset=utf-8'
     };
